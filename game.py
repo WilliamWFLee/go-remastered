18edf11955cfcb3b54cb8a43836f9793c8ed99f5
William Lee <wlee753159@gmail.com>
2020-07-09 16:16:36 +0100

    Add more setup to Go class

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from enum import Enum
+
 LINE_WIDTH = 2
 BOARD_COLOUR = (220, 181, 121)
 DEFAULT_BOARD_SIZE = 19
@@ -18,6 +20,19 @@ HOSHI_POSITIONS = {
     19: [(x, y) for x in (3, 9, 15) for y in (3, 9, 15)],
 }
 
+HOSHI_RADIUS_SCALE = 0.08
+STONE_RADIUS_SCALE = 0.46
+STAT_PANEL_WIDTH_SCALE = 8
+STAT_SCALE = {
+    "width": 0.5,
+    "height": 0.9,
+}
+
+
+class Color(Enum):
+    BLACK = 0
+    WHITE = 1
+
 
 class Go:
     """
@@ -31,3 +46,19 @@ class Go:
         self.square_width = (
             square_width if square_width is not None else DEFAULT_SQUARE_WIDTH
         )
+
+        self.hoshi_radius = int(HOSHI_RADIUS_SCALE * self.square_width)
+        self.stone_radius = int(STONE_RADIUS_SCALE * self.square_width)
+        self.board_width = self.board_size * self.square_width
+
+        self.stat_panel_width = STAT_PANEL_WIDTH_SCALE * self.square_width
+        self.stat_height = int(STAT_SCALE["height"] * self.square_width)
+        self.stat_width = int(STAT_SCALE["width"] * self.stat_panel_width)
+        self.screen_dimensions = (
+            self.board_width + self.stat_panel_width + self.square_width // 2,
+            self.board_width,
+        )
+
+        self.current_color = Color.BLACK
+        self.history = []
+        self.history_position = 0
