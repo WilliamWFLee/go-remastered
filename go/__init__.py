32685cde5b1eb29455401f7f05853ab24dd4a143
William Lee <wlee753159@gmail.com>
2020-07-27 20:09:10 +0100

    Add logging setup to __init__.py

--- go/__init__.py
+++ go/__init__.py
@@ -1,3 +1,21 @@
 # -*- coding: utf-8 -*-
 
+import logging
+import sys
+
 __version__ = "0.1.0"
+
+
+# Setup logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stderr)
+formatter = logging.Formatter(
+    "[{asctime}][{levelname}] {name}: {message}", "%Y-%m-%d %H:%M:%S", "{"
+)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+# Clean up namespace
+del logger, handler, formatter
